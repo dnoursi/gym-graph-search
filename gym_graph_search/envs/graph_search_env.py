2bf7b0885fa461ef927a382a445defe657afa64d
David Noursi <david.melons@gmail.com>
2019-02-10 16:01:30 -0600

    State space is entire set of nodes

--- gym_graph_search/envs/graph_search_env.py
+++ gym_graph_search/envs/graph_search_env.py
@@ -6,6 +6,98 @@ from gym.utils import seeding
 
 import numpy as np
 
+# Barabasi Albert Graph
+# m0 = initial clique size
+# m = number of neighbors to add to each new node
+# n = number of total desired nodes in graph
+# Begins with graph of m0, adds n-m0 nodes with m neighbors each
+def ba_graph(n, m0, m):
+    ls0 = list(range(m0))
+    edges = [ls0[:i] + ls0[i+1:] for i in ls0]
+    # Number of neighbors
+    nn = [n-1 for _ in ls0]
+    sum_nn = sum(nn)
+    for i_new in range(m0, n):
+        new = []
+        for i in range(len(edges)):
+            proba = nn[i]/sum_nn
+            if np.random.random() < proba:
+                new.append(i)
+
+                edges[i].append(i_new)
+                nn[i] += 1
+
+                sum_nn += 2
+        edges.append(new)
+        nn.append(len(new))
+        print(edges)
+    return edges
+
+def random_graph(n, m0, m):
+    return ba_graph(n, m0, m)
+
+class GraphSearchEnv(gym.Env):
+    metadata = {'render.modes':[]}
+
+    def __init__(self):
+
+        # number of nodes
+        N = 40
+        self.N = N
+        m0 = 12
+        m = 8
+
+        self.root = 0
+        self.target = m0-1
+
+        self.graph_edges = random_graph(N, m0, m)
+
+        self.observation_space = spaces.Discrete(N)
+        self.action_space = spaces.Discrete(N)
+        return
+
+    def get_action_space(self):
+        return self.graph_edges[self.current_state]
+
+    def step(self, action):
+        #print("state", self.current_state, "action", action, "edges", self.graph_edges[self.current_state])
+        if action not in self.graph_edges[self.current_state]:
+            reward = -2
+        else:
+            self.current_state = action
+            reward = -1
+
+        obs = self.current_state
+        done = (self.current_state == self.target)
+        info = {}
+        # reward: float. done: bool. info: dict.
+        # return obs, reward, done, info
+        return obs, reward, done, info
+
+    def reset(self):
+        self.current_state = self.root
+        return self.current_state
+
+    def render(self, mode='human', close=False):
+        return
+
+# Erdos Renyi Graph
+def er_graph(nnodes, proba):
+    edges = [[] for _ in range(nnodes)]
+    for i in range(nnodes):
+        for j in range(i):
+            if np.random.random() < proba:
+                edges[i].append(j)
+                edges[j].append(i)
+
+    print(edges)
+    return edges
+
+# ---
+# Currently deprecated:
+# ---
+
+
 # i has no possible neighbors remaining itself.
 # Go to j < i, and steal a neighbor for i from j
 def steal_two_neighbors(edges, i):
@@ -24,8 +116,7 @@ def steal_two_neighbors(edges, i):
 
     return edges
 
-# Not erdos renyi.. although that would be easier
-# Probably need nneighbors, nnodes even integers
+# (Probably requires nneighbors, nnodes to be even integers)
 def fixed_degree_random_graph(nneighbors, nnodes):
     remaining_neighbors = list(range(nnodes))
     edges = [[] for _ in range(nnodes)]
@@ -52,55 +143,17 @@ def fixed_degree_random_graph(nneighbors, nnodes):
         # print(edges)
     return edges
 
-# Erdos Renyi
-def er_graph(nneighbors, nnodes):
-    proba = nneighbors/nnodes
-    edges = [[] for _ in range(nnodes)]
-    for i in range(nnodes):
-        for j in range(i):
-            if np.random.random() < proba:
-                edges[i].append(j)
-                edges[j].append(i)
-
-    print(edges)
-    return edges
-
-# Barabasi Albert Graph
-def random_graph(n, nnew):
-    lsn = list(range(n))
-    edges = [lsn[:i] + lsn[i+1:] for i in lsn]
-    # Number of neighbors
-    nn = [n-1 for _ in lsn]
-    sum_nn = sum(nn)
-    for i_new in range(n, n + nnew):
-        new = []
-        for i in range(len(edges)):
-            proba = nn[i]/sum_nn
-            if np.random.random() < proba:
-                new.append(i)
-
-                edges[i].append(i_new)
-                nn[i] += 1
-
-                sum_nn += 2
-        edges.append(new)
-        nn.append(len(new))
-        print(edges)
-    return edges
-
-class GraphSearchEnv(gym.Env):
+class ConstActionSpaceSizeGraphSearchEnv(gym.Env):
     metadata = {'render.modes':[]}
 
     def __init__(self):
 
-        n = 64
-        N = n**2
         # number of neighbors of each node
-        self.n = n
+        self.n = 64
         # number of nodes (can be 2**n?)
-        self.N = N
+        self.N = self.n**2
 
-        self.graph_edges = random_graph(n, N)
+        self.graph_edges = fixed_degree_random_graph(n, N)
         #self.graph_edges = [list(np.random.choice(a = rangeN[:i] + rangeN[i+1:], size=n)) for i in rangeN]
 
         self.observation_space = spaces.Discrete(N)
